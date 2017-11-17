d92f72f91a7285306c9b93bc71aae4bbb9d377fd
Sam Hunt <s2112201@ipc.ac.nz>
2017-11-18 03:16:48 +1300

    Initial commit.

---
+++ stanford-nlp-test/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("stanford-nlp-test")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("stanford-nlp-test")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3cc761c4-5371-4b03-a734-450211c4e902")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ stanford-nlp-test/POSTaggerTest.vb
@@ -0,0 +1,34 @@
+﻿Imports java.io
+Imports java.util
+
+Imports edu.stanford.nlp.ling
+Imports edu.stanford.nlp.tagger.maxent
+
+Imports Console = System.Console
+
+Module POSTaggerTest
+
+    Sub Main()
+        Dim jarRoot = My.Application.Info.DirectoryPath + "\..\..\jar"
+        'Dim jarRoot = "..\jar"
+        Dim modelsDirectory = jarRoot + "\models"
+
+        Console.WriteLine(jarRoot)
+
+        'Loading POS Tagger
+        Dim tagger = New MaxentTagger(modelsDirectory + "\english-bidirectional-distsim.tagger")
+
+        Dim Text As String
+
+        Do
+            Text = Console.ReadLine()
+            Dim sentences = MaxentTagger.tokenizeText(New StringReader(Text)).toArray()
+            For Each sentence As ArrayList In sentences
+                Dim taggedSentence = tagger.tagSentence(sentence)
+                Console.WriteLine(SentenceUtils.listToString(taggedSentence, False))
+            Next
+        Loop While Text.Length() > 0
+
+    End Sub
+
+End Module
